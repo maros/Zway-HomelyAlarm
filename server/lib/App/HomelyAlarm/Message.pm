ebad8edcedc97115fa81b3d851c25de952de45ed
Maros Kollar <maros@k-1.com>
2015-12-15 21:48:38 +0100

    Basic message class

--- server/lib/App/HomelyAlarm/Message.pm
+++ server/lib/App/HomelyAlarm/Message.pm
@@ -1,6 +1,5 @@
 package App::HomelyAlarm::Message {
     use 5.016;
-    use warnings;
     
     use Moose;
     
@@ -19,7 +18,8 @@ package App::HomelyAlarm::Message {
     has 'recipients' => (
         is              => 'rw',
         isa             => 'ArrayRef[App::HomelyAlarm::Recipients]',
-        required        => 1
+        required        => 1,
+        default         => sub { [] },
         #traits          => ['Array'],
         #handles          => {
         #    'add_recipient' => 'push'
@@ -31,4 +31,26 @@ package App::HomelyAlarm::Message {
         isa             => 'Str',
         required        => 1
     );
+    
+    sub add_recipient {
+        my ($self,%params) = @_;
+        
+        my $recipient = App::HomelyAlarm::Recipients->new(
+            (
+                map { $_ => $params{$_} }
+                grep { defined $params{$_} }
+                qw(email voice sms)
+            ),
+            message     => $self,
+        );
+        push (@{$self->recipients},$recipient);
+        return $recipient;
+    }
+    
+    sub process {
+        my ($self) = @_;
+        foreach my $recipient (@{$self->recipients}) {
+            $recipient->process();
+        }
+    }
 }
